08d8596f20f77a055da2f43e263730dfa5d5ee4f
Vinicius Veiga <veiga201@hotmail.com>
2019-04-19 17:36:24 -0300

    Aplicando layout

--- TCC/App_Start/BundleConfig.cs
+++ TCC/App_Start/BundleConfig.cs
@@ -26,6 +26,14 @@ namespace TCC
                       "~/Content/AdminLTE/css/alt/AdminLTE-select2.css",
                       "~/Content/AdminLTE/css/alt/AdminLTE-without-plugins.css",
                       "~/Content/AdminLTE/css/skins/_all-skins.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Components").Include(
+                      "~/Content/Components/bootstrap/dist/css/bootstrap.min.css",
+                      "~/Content/Components/font-awesome/css/font-awesome.min.css",
+                      "~/Content/Components/morris.js/morris.css",
+                      "~/Content/Components/jvectormap/jquery-jvectormap.css",
+                      "~/Content/Components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css",
+                      "~/Content/Components/bootstrap-daterangepicker/daterangepicker.css"));
         }
     }
 }
