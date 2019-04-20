c75ff80128d2861bb570c19381c942e303f80ba6
Vinicius Veiga <veiga201@hotmail.com>
2019-04-19 22:46:04 -0300

    Scripts do DataBase

---
+++ TCC.Entity/DataBase/dbo.TCC_USER.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TCC_USER] (
+    [ID_N]            INT           IDENTITY (1, 1) NOT NULL,
+    [NAME_C]          VARCHAR (50)  NOT NULL,
+    [EMAIL_C]         VARCHAR (250) NOT NULL,
+    [PASSWORD_C]      NCHAR (150)   NOT NULL,
+    [ADDED_DATE_D]    DATETIME      NOT NULL,
+    [MODIFIED_DATE_D] DATETIME      NULL,
+    [ACTIVE_B]        BIT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID_N] ASC)
+);
+
