25bf5e60aab4ca8ff7bd3f8dc30a65b4007c8c0a
Vinicius Veiga <veiga201@hotmail.com>
2019-05-22 13:39:15 -0300

    User Story   - Adicionando escolha de projeto no user story   - Adicionando escolha de atores no user story

---
+++ TCC.UI/Controllers/UserStoryController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TCC.BusinessLayer.Admin;
+
+namespace TCC.UI.Controllers
+{
+    public class UserStoryController : Controller
+    {
+        #region Etapa 1
+
+        public ActionResult Index() => View("FirstStep");
+
+        #endregion
+
+        #region Etapa 2
+
+        public ActionResult SecondStep() => View("SecondStep");
+
+        #endregion
+    }
+}
