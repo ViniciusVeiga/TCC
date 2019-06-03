0c78519dc092fb0dcd8750b1eb090caac18b7573
Vinicius Veiga <veiga201@hotmail.com>
2019-06-02 21:27:53 -0300

    Home   - Colocando conteudo na home   - Mudando algumas coisas no CSS

--- TCC.UI/Content/Scripts/custom-landing.page.js
+++ TCC.UI/Content/Scripts/custom-landing.page.js
@@ -11,9 +11,17 @@ function defineHeaderColor() {
     var header = $(".skin-blue .main-header .navbar");
 
     if ($(window).scrollTop() < $(window).height() - header.height()) {
-        header.css("background-color", "transparent");
+        header.addClass("transparent");
     }
     else {
-        header.css("background-color", "#3c8dbc");
+        header.removeClass("transparent");
     }
 }
+
+$("#KnowMore").click(function () {
+    var header = $(".skin-blue .main-header .navbar");
+
+    $('html, body').animate(
+        { scrollTop: $(window).height() - header.height() }, 500, 'linear'
+    );
+});
