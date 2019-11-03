16d1e38f70d6240dba97a274a77898af42bed891
Vinicius Veiga <veiga201@hotmail.com>
2019-11-03 16:39:18 -0300

    Exportar   - Criando exportar do user story

--- TCC.UI/Content/Scripts/Postits/scripts-postit.js
+++ TCC.UI/Content/Scripts/Postits/scripts-postit.js
@@ -43,3 +43,34 @@ function writeMessage(text) {
 function hideMessage() {
     $('.board-message').fadeOut();
 }
+
+function renderPDF(url, canvasContainer, options) {
+    options = options || { scale: 1.4 };
+
+    function renderPage(page) {
+        var viewport = page.getViewport(options.scale);
+        var wrapper = document.createElement("div");
+        wrapper.className = "canvas-wrapper";
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        var renderContext = {
+            canvasContext: ctx,
+            viewport: viewport
+        };
+
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        wrapper.appendChild(canvas)
+        canvasContainer.appendChild(wrapper);
+
+        page.render(renderContext);
+    }
+
+    function renderPages(pdfDoc) {
+        for (var num = 1; num <= pdfDoc.numPages; num++)
+            pdfDoc.getPage(num).then(renderPage);
+    }
+
+    PDFJS.disableWorker = true;
+    PDFJS.getDocument(url).then(renderPages);
+}
